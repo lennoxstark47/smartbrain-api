a0b5fc61e2e28eb185f46d510acb76a97848607a
Lennox Stark <stark47@Lennoxs-iMac.local>
2021-05-23 21:12:24 +0530

    signin and register

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
+const BodyParser = require('body-parser');
 const app = express();
-
+app.use(BodyParser.json());
 const database = {
 	users: [
 		{
@@ -12,10 +12,44 @@ const database = {
 			entries: 0,
 			joined: new Date(),
 		},
+		{
+			id: 124,
+			name: 'sally',
+			email: 'sally@gmail.com',
+			password: '654321',
+			entries: 0,
+			joined: new Date(),
+		},
 	],
 };
+app.get('/', (req, res) => {
+	res.json(database.users);
+});
 app.post('/signin', (req, res) => {
-	res.json('signing in...');
+	if (
+		req.body.email === database.users[0].email &&
+		req.body.password ===
+			database.users[0].password
+	) {
+		res.json('success');
+	} else {
+		res.status(400).json('error logging in');
+	}
+});
+
+app.post('/register', (req, res) => {
+	const { email, name, password } = req.body;
+	database.users.push({
+		id: 125,
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date(),
+	});
+	res.json(
+		database.users[database.users.length - 1]
+	);
 });
 
 app.listen(3000, () => {
