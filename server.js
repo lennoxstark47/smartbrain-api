29a44dc95c187267cf519cf2a6bd92bcf690a9e0
Stark 47 <twisamhaldar@gmail.com>
2021-05-26 20:02:54 +0530

    added signin and register endpoint

--- server.js
+++ server.js
@@ -1,21 +1,56 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-
+app.use(bodyParser.json());
 const database = {
 	users: [
 		{
 			id: 123,
 			name: 'john',
 			email: 'john@gmail.com',
-			password: '123456',
+			password: 'cookies',
+			entries: 0,
+			joined: new Date(),
+		},
+		{
+			id: 124,
+			name: 'sally',
+			email: 'sally@gmail.com',
+			password: 'bananas',
 			entries: 0,
 			joined: new Date(),
 		},
 	],
 };
+app.get('/', (req, res) => {
+	res.json(database.users);
+});
 app.post('/signin', (req, res) => {
-	res.json('signing in...');
+	if (
+		req.body.email === database.users[0].email &&
+		req.body.password ===
+			database.users[0].password
+	) {
+		res.json('success');
+	} else {
+		res.status(400).json('error loggin in');
+	}
+});
+
+app.post('/register', (req, res) => {
+	const { name, email, password } = req.body;
+	database.users.push({
+		id: 125,
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date(),
+	});
+	res.json(
+		database.users[database.users.length - 1]
+	);
 });
 
 app.listen(3000, () => {
