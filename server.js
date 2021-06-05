d709c125329d14a0b6cd042f4779c73f7e088655
Lennox Stark <stark47@Lennoxs-iMac.local>
2021-06-05 22:08:46 +0530

    security patch 1 (clarifai)

--- server.js
+++ server.js
@@ -2,17 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const bcrypt = require('bcrypt-nodejs');
+const Clarifai = require('clarifai');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 const knex = require('knex');
+const faceApp = new Clarifai.App({
+	apiKey: 'c14191b446b14919afd059c9a0666edf',
+});
 
 const db = knex({
 	client: 'pg',
 	connection: {
 		host: '127.0.0.1',
-		user: 'postgres',
-		password: '4774',
+		user: 'stark47',
+		password: '',
 		database: 'smartbrain',
 	},
 });
@@ -21,8 +25,13 @@ app.get('/', (req, res) => {
 	res.json(database.users);
 });
 app.post('/signin', (req, res) => {
-	db
-		.select('hash', 'email')
+	const { email, password } = req.body;
+	if (!email || !password) {
+		return res
+			.status(400)
+			.json('Incorrect Form Submission');
+	}
+	db.select('hash', 'email')
 		.from('login')
 		.where('email', '=', req.body.email)
 		.then((data) => {
@@ -39,7 +48,9 @@ app.post('/signin', (req, res) => {
 						res.json(user[0]);
 					})
 					.catch((err) => {
-						res.status(400).json('Unable to get user');
+						res
+							.status(400)
+							.json('Unable to get user');
 					});
 			} else {
 				res.status(400).json('wrong credentials');
@@ -52,41 +63,42 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
 	const { name, email, password } = req.body;
+	if (!name || !email || !password) {
+		return res
+			.status(400)
+			.json('Incorrect Form Submission');
+	}
 	const hash = bcrypt.hashSync(password);
-	db
-		.transaction((trx) => {
-			trx
-				.insert({
-					hash: hash,
-					email: email,
-				})
-				.into('login')
-				.returning('email')
-				.then((loginEmail) => {
-					return trx('users')
-						.returning('*')
-						.insert({
-							name: name,
-							email: loginEmail[0],
-							joined: new Date(),
-						})
-						.then((user) => {
-							res.json(user[0]);
-						});
-				})
-				.then(trx.commit)
-				.catch(trx.rollback);
-		})
-
-		.catch((err) =>
-			res.status(400).json('Email already exists')
-		);
+	db.transaction((trx) => {
+		trx
+			.insert({
+				hash: hash,
+				email: email,
+			})
+			.into('login')
+			.returning('email')
+			.then((loginEmail) => {
+				return trx('users')
+					.returning('*')
+					.insert({
+						name: name,
+						email: loginEmail[0],
+						joined: new Date(),
+					})
+					.then((user) => {
+						res.json(user[0]);
+					});
+			})
+			.then(trx.commit)
+			.catch(trx.rollback);
+	}).catch((err) =>
+		res.status(400).json('Email already exists')
+	);
 });
 
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params;
-	db
-		.select('*')
+	db.select('*')
 		.from('users')
 		.where({ id: id })
 		.then((user) => {
@@ -101,8 +113,19 @@ app.get('/profile/:id', (req, res) => {
 		);
 });
 
+app.post('/imageurl', (req, res) => {
+	faceApp.models
+		.predict(
+			Clarifai.FACE_DETECT_MODEL,
+			req.body.input
+		)
+		.then((data) => res.json(data))
+		.catch(console.log);
+});
+
 app.put('/image', (req, res) => {
 	// let found = false;
+
 	const { id } = req.body;
 	db('users')
 		.where('id', '=', id)
